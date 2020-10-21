f48f14228d83e8b0ef67a4019bfc7f8e56c3dd45
Chris Moreton <chris@netsensia.com>
2020-10-21 23:10:00 +0100

    Refactoring

--- src/main/java/com/netsensia/rivalchess/player/MatchPlayer.kt
+++ src/main/java/com/netsensia/rivalchess/player/MatchPlayer.kt
@@ -4,15 +4,34 @@ import com.google.gson.Gson
 import com.netsensia.rivalchess.service.cuteChess
 import com.netsensia.rivalchess.utils.JmsReceiver
 import com.netsensia.rivalchess.utils.JmsSender
-import com.netsensia.rivalchess.utils.getEngines
+import com.netsensia.rivalchess.utils.getFile
 import com.netsensia.rivalchess.vie.model.EngineSettings
 import com.netsensia.rivalchess.vie.model.MatchResult
 import java.io.File
+
+fun getEngine(engineVersion: String) {
+    val s3Name = "rivalchess-${engineVersion}-1.jar"
+    val filePath = "/tmp/$s3Name"
+    getFile("rivalchess-jars", s3Name, filePath)
+}
+
+fun getOpeningBook(openingBook: String) {
+    val s3Name = "${openingBook}.jar"
+    val filePath = "/tmp/$s3Name"
+    getFile("rivalchess-openings", s3Name, filePath)
+}
+
+fun getEngines(engine1: String, engine2: String) {
+    getEngine(engine1)
+    getEngine(engine2)
+}
 
 fun game(matchRequest: EngineSettings): Boolean {
     val engine1 = matchRequest.engine1.version
     val engine2 = matchRequest.engine2.version
     getEngines(engine1, engine2)
+    getOpeningBook(matchRequest.engine1.openingBook)
+    getOpeningBook(matchRequest.engine2.openingBook)
     val result = cuteChess(matchRequest)
     println(result)
     val pgn = File("/tmp/out.pgn").readText()
