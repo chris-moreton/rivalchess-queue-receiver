8060c93d1e2afb9c53020091d300dd5b05e34c27
Chris Moreton <chris@netsensia.com>
2020-10-21 16:07:24 +0100

    Use utils library

--- src/main/java/com/netsensia/rivalchess/generator/MatchPlayer.kt
+++ src/main/java/com/netsensia/rivalchess/generator/MatchPlayer.kt
@@ -1,10 +1,10 @@
 package com.netsensia.rivalchess.generator
 
 import com.google.gson.Gson
-import com.netsensia.rivalchess.service.JmsReceiver
-import com.netsensia.rivalchess.service.JmsSender
 import com.netsensia.rivalchess.service.cuteChess
-import com.netsensia.rivalchess.service.getEngines
+import com.netsensia.rivalchess.utils.JmsReceiver
+import com.netsensia.rivalchess.utils.JmsSender
+import com.netsensia.rivalchess.utils.getEngines
 import com.netsensia.rivalchess.vie.model.EngineSettings
 import com.netsensia.rivalchess.vie.model.MatchResult
 import java.io.File
@@ -17,14 +17,14 @@ fun game(matchRequest: EngineSettings): Boolean {
     println(result)
     val pgn = File("/tmp/out.pgn").readText()
     val matchResult = MatchResult(matchRequest, pgn)
-    JmsSender.send(matchResult)
+    JmsSender.send("MatchResults", matchResult)
     return true
 }
 
 fun main() {
     do {
         val gson = Gson()
-        val message = JmsReceiver.receive()
+        val message = JmsReceiver.receive("MatchRequests")
         val matchRequest = gson.fromJson(message, EngineSettings::class.java)
         println("Match between ${matchRequest.engine1} and ${matchRequest.engine2}")
     } while (game(matchRequest))
